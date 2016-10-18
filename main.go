9a691cd4e1c4fa33d84ccd1703f091fce2a390bc
Tobias Schwab <tobias.schwab@dynport.de>
2016-10-18 15:21:52 +0200

    add little server

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,5 +16,24 @@ func main() {
 }
 
 func run(l *log.Logger) error {
-	return nil
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	if !strings.Contains(port, ":") {
+		port = "0.0.0.0:" + port
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/clicked", clicked)
+	l.Printf("running on port %q", port)
+	return http.ListenAndServe(port, mux)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, `<html><body><a href="#" onclick="javascript:document.location = '/clicked';">Click me</a>`)
+}
+
+func clicked(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, `<html><body><div id="clicked"><h2>you clicked!</h2></div>`)
 }
